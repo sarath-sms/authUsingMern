6117743cf8c1c2993e8cb81fd8e003ada9fda8ce
Sarath_Kumar_S <sarathmca2016@gmail.com>
2025-07-24 01:39:08 +0530

    final

---
+++ client/src/App.js
@@ -0,0 +1,16 @@
+import { Route, Routes } from "react-router-dom";
+import { Login, Signup, Home } from "./pages/index";
+
+function App() {
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default App;
